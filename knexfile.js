2dbd6806b11e52a2e7a95b9dcea2ccf3b2238442
sleon204 <sleon87@gmail.com>
2023-08-02 07:36:21 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "https://we-love-movies-backend-zvy7.onrender.com",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
