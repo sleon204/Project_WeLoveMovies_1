f89886b0175520434444db92ad7e30c4f498dad4
sleon204 <sleon87@gmail.com>
2023-08-02 08:15:13 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
